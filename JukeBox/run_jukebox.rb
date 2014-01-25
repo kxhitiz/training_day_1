887344234040e4ce84e88e110c0c972775d1cac0
Sundar M. Vaidya <sundar@cloudfactory.com>
2014-01-25 17:14:41 +0545

    Script to run JukeBox.

---
+++ JukeBox/run_jukebox.rb
@@ -0,0 +1,25 @@
+
+require './JukeBox.rb'
+
+j = JukeBox.new()
+j.get_current
+
+j.next
+j.prev
+j.next
+
+j.get_current
+
+j.next
+j.next
+j.next
+j.next
+j.next
+j.next
+
+j.prev
+j.prev
+j.prev
+j.prev
+j.prev
+j.prev
