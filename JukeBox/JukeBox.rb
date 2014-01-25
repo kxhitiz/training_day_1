c108f4f7e89797b311f977280c73ed5959fc5e23
Sundar M. Vaidya <sundar@cloudfactory.com>
2014-01-25 16:52:21 +0545

    Adds method to get current item.

--- JukeBox/JukeBox.rb
+++ JukeBox/JukeBox.rb
@@ -5,4 +5,10 @@ class JukeBox
 		@current = 0
 		@list = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5']
 	end
+
+	def get_current
+		puts 'Getting current item'
+		puts @list[@current]
+	end
+
 end
