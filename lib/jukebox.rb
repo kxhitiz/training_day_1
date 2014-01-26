11e9314710bb023a4c4b7fe4966ad237f2b42a4f
josisusan <josisusan@gmail.com>
2014-01-26 09:41:51 +0545

    Add current method to display current song on playlist.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,7 +1,15 @@
 class Jukebox
   attr_accessor :playlist
   def initialize(*args)
-    playlist = *args
+    @playlist = *args
+  end
+
+  def current
+    if playlist.length <= 0
+      "There are no songs on the playlist"
+    else
+      playlist.first  
+    end
   end
   
   
