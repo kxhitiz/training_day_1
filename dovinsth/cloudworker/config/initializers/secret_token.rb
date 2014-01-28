4d4d85d616c9a48f4994674586e3c4dfcec2d397
dovinsth <laxman.shrestha@cloudfactory.com>
2014-01-28 17:05:46 +0545

    New project creation

---
+++ dovinsth/cloudworker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudworker::Application.config.secret_token = '9ceb446019491455762a3a88c81f61c6b09aa7e4932587ff7c24221c50df419b1008808891f0ec90d3f4bfdffae5fb5b73117750189c99f2a3fda96f3f3e2d58'
