2959ace21de5c5a5f0fa69bbad9a9bdec5a49753
bhanubhakta <bsbhanu169@gmail.com>
2014-01-28 08:40:25 +0545

    Initial commit

---
+++ address.rb
@@ -0,0 +1,8 @@
+class Address
+	attr_accessor :country, :city, :zip_code
+  def initialize(options = {})
+    @country = options[:country]
+    @city = options[:city]
+    @zip_code = options[:zip_code]
+  end
+end
