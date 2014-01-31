c42ea22be732ab1c6a2f8aab1b6f7466f7bbb121
dovinsth <laxman.shrestha@cloudfactory.com>
2014-01-31 11:04:49 +0545

    eligibility test function added into validator module

--- dovinsth/validator.rb
+++ dovinsth/validator.rb
@@ -3,4 +3,13 @@ module Validator
 	CITY = "KATHMANDU"
 	MIN_TEST_SCORE = 80.0
 
+	def calc_avg_test_score
+		test_scores.inject(0) {|total_score, score| total_score + score}.to_f /  test_scores.length
+	end
+
+	def is_eligible?
+		avg_test_score = calc_avg_test_score
+		(age >= MIN_AGE && avg_test_score >= MIN_TEST_SCORE && ((address.city.upcase) == CITY)) ? true : false
+	end
+
 end
