6af183cb84bdc4acab70a722dfa1aa4aaea3d577
Sundar M. Vaidya <sundar@cloudfactory.com>
2014-02-02 08:51:49 +0545

    Adds attribute :scores and method test_scores() to assign :scores.

--- Worker/Worker.rb
+++ Worker/Worker.rb
@@ -1,7 +1,10 @@
 class Worker
-	attr_accessor :name, :age, :test_scores, :gender
+	attr_accessor :name, :age, :test_scores, :gender, :scores
 
 	include Address
 
+	def test_scores(scores)
+		@scores = scores
+	end
 
 end
