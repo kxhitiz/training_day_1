a732a3a0a7487f7ecbeb557f70aef2afc3d6d0d6
Sundar M. Vaidya <sundar@cloudfactory.com>
2014-02-02 09:18:27 +0545

    isEligibleToBeCloudworker(): Checks worker's eligibility to be a cloud worker.

--- Worker/Worker.rb
+++ Worker/Worker.rb
@@ -1,10 +1,18 @@
-class Worker
-	attr_accessor :name, :age, :test_scores, :gender, :scores
+require './Address.rb'
 
+class Worker
 	include Address
+
+	attr_accessor :name, :age, :test_scores, :gender, :scores
 
 	def test_scores(scores)
 		@scores = scores
+	end
+
+	def isEligibleToBeCloudworker()
+		(@city == "Kathmandu" and 
+			@age > 18 and 
+			(@scores.inject(0.0){ |sum, score| sum + score}) > 80)
 	end
 
 end
